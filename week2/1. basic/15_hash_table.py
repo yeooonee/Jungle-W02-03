85d56842dfb1d58490bafec6eb53efc1b368ff97
yeooonee <skylarlee1003@gmail.com>
2026-09-01 15:20:09 +0900

    15 solve

--- week2/1. basic/15_hash_table.py
+++ week2/1. basic/15_hash_table.py
@@ -41,11 +41,12 @@ def manage_grades(students):
         평균, 최고점 학생 이름, 최고점
     """
     # TODO: 평균 점수 계산
-    pass
+    average = sum(students.values()) / len(students)
     
     
     # TODO: 최고 점수 학생 찾기
-    pass
+    top_student = max(students, key=students.get)
+    top_score = max(students.values())
     
     return average, top_student, top_score
 
@@ -61,7 +62,7 @@ def find_student_score(students, name):
         점수 (없으면 None)
     """
     # TODO: students에서 name 찾기
-    pass
+    return students.get(name)
 
 # 테스트 케이스
 if __name__ == "__main__":
