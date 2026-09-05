7189b5aa78ccf623300f0eca8db1e5b20a1b1987
yeooonee <skylarlee1003@gmail.com>
2026-09-05 14:02:31 +0900

    06 solve

--- week3/1. basic/06_dp_fibonacci.py
+++ week3/1. basic/06_dp_fibonacci.py
@@ -82,16 +82,21 @@ def fibonacci_memo(n, memo=None):
         n번째 피보나치 수
     """
     # TODO: memo가 None이면 빈 딕셔너리로 초기화
-    pass
+    if memo is None:
+        memo = {}
     
     # TODO: base case 
-    pass
+    if n == 0 or n == 1:
+        return n
     
     # TODO: 이미 계산한 값이 memo에 있으면 반환
-    pass
+    if n in memo:
+        return memo[n]
     
     # TODO: 재귀 호출하여 계산하고 memo에 저장
-    pass
+    else:
+        val = fibonacci_memo(n - 1,memo) + fibonacci_memo(n - 2, memo)
+        memo[n] = val
     
     return memo[n]
 
